ce6fecbc310cf4095d0350145840f6219634f152
Katrina Brinson <kbrinso@gmail.com>
2018-01-10 22:23:13 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(board="")
+def display_board(board="   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n")
+  
